129af026982d90bb061d21450d7a8f6ff02efaf7
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-04-04 19:10:42 -0300

    Método Get uma pessoa

--- src/main/java/com/vipdsilva/app/ws/controller/PeopleController.java
+++ src/main/java/com/vipdsilva/app/ws/controller/PeopleController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import com.vipdsilva.app.ws.repository.PeopleRepository;
 import com.vipdsilva.app.ws.service.PeopleService;
 import com.vipdsilva.app.ws.entities.People;
 import com.vipdsilva.app.ws.model.request.PeopleDtoRequestModel;
+import com.vipdsilva.app.ws.model.request.UpdatePeopleRequestModel;
 import com.vipdsilva.app.ws.model.response.PeopleDtoResponseModel;
 
 @RestController
@@ -34,6 +37,14 @@ public class PeopleController {
 		List<People> pessoas = peopleRepository.findAll();
 		
 		return new ResponseEntity<>(pessoas, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{peopleId}")
+	public ResponseEntity<PeopleDtoResponseModel> showPerson(@PathVariable Integer peopleId) {
+		People person = peopleRepository.getById(peopleId);
+		PeopleDtoResponseModel response = person.toResponseDto();
+		
+		return new ResponseEntity<PeopleDtoResponseModel>(response, HttpStatus.OK);
 	}
 	
 	@PostMapping
