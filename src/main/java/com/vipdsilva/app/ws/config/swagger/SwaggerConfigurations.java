6183babc5169063c3a2ad731bf7a06de16dd441b
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-05-17 14:30:49 -0300

    Refatoração

--- src/main/java/com/vipdsilva/app/ws/config/swagger/SwaggerConfigurations.java
+++ src/main/java/com/vipdsilva/app/ws/config/swagger/SwaggerConfigurations.java
@@ -2,24 +2,16 @@ package com.vipdsilva.app.ws.config.swagger;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-
-import com.vipdsilva.app.ws.entities.User;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -46,21 +38,6 @@ public class SwaggerConfigurations {
                     .build()
             ));
     }
-
-/*     private ApiKey apiKey() { 
-        return new ApiKey("JWT", "Authorization", "header"); 
-    }
-
-    private SecurityContext securityContext() { 
-        return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
-    } 
-    
-    private List<SecurityReference> defaultAuth() { 
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
-        authorizationScopes[0] = authorizationScope; 
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
-    } */
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
--- src/main/java/com/vipdsilva/app/ws/entities/People.java
+++ src/main/java/com/vipdsilva/app/ws/entities/People.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.util.*;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
